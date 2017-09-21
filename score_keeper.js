fa66bf284aa89635c7af021a2f872493637a1fe4
Alex Law <achlawuk@gmail.com>
2017-09-21 12:29:17 +0100

    Update Player 2 button incrementation

--- score_keeper.js
+++ score_keeper.js
@@ -1,9 +1,16 @@
 var player1Button = document.querySelector("#player1");
 var player2Button = document.querySelector("#player2");
 var player1Display = document.querySelector("#player1Display");
+var player2Display = document.querySelector("#player2Display");
 var player1Score = 0;
+var player2Score = 0;
 
 player1Button.addEventListener("click", function(){
 	player1Score++;
 	player1Display.textContent = player1Score;
+});
+
+player2Button.addEventListener("click", function(){
+	player2Score++;
+	player2Display.textContent = player2Score;
 });
