77512a5de6f9f539a9aedc6cbba370e3d1590115
Alex Law <achlawuk@gmail.com>
2017-09-22 17:27:21 +0100

    Update Player 1 score limit

--- score_keeper.js
+++ score_keeper.js
@@ -4,13 +4,20 @@ var player1Display = document.querySelector("#player1Display");
 var player2Display = document.querySelector("#player2Display");
 var player1Score = 0;
 var player2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 player1Button.addEventListener("click", function(){
-	player1Score++;
-	player1Display.textContent = player1Score;
+  if(!gameOver){
+    player1Score++;
+    if(player1Score === winningScore){
+        gameOver = true;
+      }
+      player1Display.textContent = player1Score;
+  }
 });
 
 player2Button.addEventListener("click", function(){
-	player2Score++;
-	player2Display.textContent = player2Score;
+  player2Score++;
+  player2Display.textContent = player2Score;
 });
