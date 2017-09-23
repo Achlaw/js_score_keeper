25080b59d07b9f1d0aaeb0fd0a697752db278d23
Alex Law <achlawuk@gmail.com>
2017-09-23 12:06:41 +0100

    Update winningScoreDisplay

--- score_keeper.js
+++ score_keeper.js
@@ -1,3 +1,4 @@
+var scoreInput = document.querySelector("input[type='number']");
 var player1Button = document.querySelector("#player1");
 var player2Button = document.querySelector("#player2");
 var resetButton = document.querySelector("#reset");
@@ -5,6 +6,7 @@ var player1Display = document.querySelector("#player1Display");
 var player2Display = document.querySelector("#player2Display");
 var player1Display2 = document.querySelector("#player1Display2");
 var player2Display2 = document.querySelector("#player2Display2");
+var winningScoreDisplay = document.querySelector("#winningScoreDisplay");
 var player1Score = 0;
 var player2Score = 0;
 var gameOver = false;
@@ -45,3 +47,7 @@ resetButton.addEventListener("click", function(){
   player2Display2.classList.remove("winner");
   gameOver = false;
 });
+
+scoreInput.addEventListener("change", function(){
+  winningScoreDisplay.textContent = scoreInput.value;
+})
